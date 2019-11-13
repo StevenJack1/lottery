5db89020d3bf75a7b65c29f6e71d5f26b8206562
StevenJack <3297573343@qq.com>
2019-11-13 10:37:33 +0800

    初始化

---
+++ js/member.js
@@ -0,0 +1,39 @@
+var member = [
+  {
+    "phone": "2019141102",
+    "name": "严泽凡"
+  },
+  {
+    "phone": "nnnn",
+    "name": "nnn"
+  },
+  {
+    "phone": "hhhh",
+    "name": "hhh"
+  },
+  {
+    "phone": "www",
+    "name": "www"
+  },
+  {
+    "phone": "2019141101",
+    "name": "zzw"
+  },
+  {
+    "phone": "xxxxx",
+    "name": "xxxx"
+  },
+  {
+    "phone": "qqqq",
+    "name": "qqqq"
+  },
+  {
+    "phone": "zzzz",
+    "name": "zzzzz"
+  },
+  {
+    "phone": "ssss",
+    "name": "ssss"
+  },
+  
+]
